5b3e095a74afc44d3778d9379626679dc961a47e
Hazem Khaled <hazemkhaled3443@gmail.com>
2022-10-22 15:29:05 +0200

    update hello file

--- hello.cpp
+++ hello.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+void test1();
+
 int main()
 {
+    
     cout << "Hello World! from latest modifications" << endl;
+    test1();
+    
     return 0;
+}
+
+void test1(){
+        cout << "test1 " << endl
 }
